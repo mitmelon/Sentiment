9535c7d8eb7a67fe7131d0c33bec5aa1993bd7ef
Manomite <support@mitnets.com>
2020-09-29 07:51:37 -0700

    license edit

--- Sentiment/Sentiment.php
+++ Sentiment/Sentiment.php
@@ -1,25 +1,10 @@
 <?php
 namespace Sentiment;
-
-/*
-  Sentiment is a Naive Bayes classifier to calculate sentiment. The program
-  uses a database of words categorised as positive, negative or neutral
-
-  Copyright (C) 2020  Mitnets Technologies
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the MIT License
-  along with this program.
-
+/**
+ * Sentiment is a sentiment classifier. It uses a model of words that are categorised as positive, negative or neutral, and a naive bayes algorithm to calculate sentiment. To improve accuracy, Sentiment removes 'noise' words
+ *
+ *  @author Mitnets Technologies <support@mitnets.com>
+ *  @version 1.0.0
  */
 
 class Sentiment
